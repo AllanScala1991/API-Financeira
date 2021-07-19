16efe148fe1c4d865d25a534842c5dfbfa71c301
Allan Scala <allanscala@MacBook-Pro-de-Allan.local>
2021-07-19 16:47:08 -0300

    adjuste db route

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const connection = new Sequelize({
-    dialect: 'sqlite',
-    storage: __dirname + '/database.db'
+const connection = new Sequelize('heroku_66d4f66c8c98815', 'b686a69cf5ae4d','19d1845f', {
+    host: 'us-cdbr-east-04.cleardb.com',
+    dialect: 'mysql'
+    //storage: __dirname + '/database.db'
 })
 
 
